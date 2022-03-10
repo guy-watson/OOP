d7c2a55b2cdf4f32ee353aca69bb6b5abc8130ba
guy-watson <95031920+guy-watson@users.noreply.github.com>
2022-03-10 13:46:46 +0000

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,16 +1,17 @@
 package cycling;
 
 public class Team {
+
+  private int teamId;
+  private String name;
+  private String description;
+  private static int count = 0;
     
-    private int teamId;
-    private String name;
-    private String description;
-    private static int count = 0;
-    
-    Team(String name, String description) {
-    this.name = name;
-    this.description = description;
-    count++;
-    Team.count =  teamId;
-    }
+  Team(String name, String description) {
+  this.name = name;
+  this.description = description;
+  count++;
+  teamId = count;
+  }
 }
+
