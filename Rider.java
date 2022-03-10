bec3a51a5d84b85ec2fa9f1c83f23d2d61f6f040
Alex Gulliver <92324769+AlexGulliver@users.noreply.github.com>
2022-03-10 13:51:36 +0000

    Add files via upload

--- Rider.java
+++ Rider.java
@@ -2,7 +2,17 @@ package cycling;
 
 public class Rider {
 
+    private String name;
     private int yearOfBirth;
     private int riderId;
+    private int teamId;
+    private static int count = 0;
+
+    Rider(String name, int yearOfBirth, int teamId) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.teamId = teamId;
+        count++;
+        riderId = count;
 
 }
