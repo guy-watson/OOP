2b50b3debcaab25f29e8f665fa89df02392f6550
guy-watson <95031920+guy-watson@users.noreply.github.com>
2022-03-10 13:27:02 +0000

    Update Team.java

--- Team.java
+++ Team.java
@@ -5,5 +5,12 @@ public class Team {
     private int teamId;
     private String name;
     private String description;
-
+    private static int count = 0;
+    
+    Team(String name, String description) {
+    this.name = name;
+    this.description = description;
+    count++;
+    Team.count =  teamId;
+    }
 }
