5bc36e986cffb87f2ada5671d0d6e9b7ad399042
guy-watson <95031920+guy-watson@users.noreply.github.com>
2022-03-31 12:34:58 +0100

    Update Race.java

--- Race.java
+++ Race.java
@@ -2,23 +2,44 @@ package cycling;
 
 import java.util.ArrayList;
 import java.util.List;
-// We need to create an array of raceIds
-// Each time a new race is created a unique race id will be generated and we need to add this to the array
-// The array will start off as an arraylist so the size can be changed and then turned back into an array so it can be called from the portal
-// ArrayList<String> cars = new ArrayList<String>();
-public class Race {
 
-    private String name;
-    private String description;
-    private int raceId;
-    private int numberOfStages;
-    private double totalLength;
+/**
+ * <h1>Race.java</h1>
+ * 
+ * Race is an object class that contains the constructor, attributes, methods, getters and setters for the race object. 
+ *<p>
+ *	
+ *	
+ *<p>
+ * 
+ *
+ */
+ 
+public class Race {
+	// Attributes
+
+    private String name; // name of the race
+    private String description; // description of the race
+    private int raceId;	// unique id of the race
+    private int numberOfStages; // number of stages in the race
+    private double totalLength; // total length of all stages in the race
     //private Stage[] stageArray = new Stage[1000];
-    private List<Stage> raceStageList = new ArrayList<Stage>();
+    private List<Stage> raceStageList = new ArrayList<Stage>(); //List containing all of the stages in the race object
     //raceStageArrayList is an array that contains stages for each individual race object
     //private final ArrayList<Stage> raceStageArrayList;
-    private static int count = 0;
+    private static int count = 0; // count allows each rider to have an incrementing id
     
+    // Constructor 
+    
+    /**
+     * <h1>Race constructor</h1>
+     * Race constructor creates a race object. It assigns the race with an 
+     * incremented raceId using the count attribute/variable. The race object contains stage objects,
+     * these in turn contain segments.
+     * 
+     * @param name The name of the race
+     * @param description The description of the race
+     */
     Race(String name, String description) {
         // Needs testing
         // Need to add race to race array list
@@ -28,69 +49,96 @@ public class Race {
         raceId = count; 
     }
 
-    Race RaceEmpty() {
-        count++;
-        raceId = count;
-        return null;
-    }
-
     // Getters
-
+    
+    /**
+     * Gets the race name
+     * @return name A string representing the name of the race
+     */
     public String getName() {
         return name;
     }
-
+    
+    /**
+     * Gets the description of the race
+     * @return description A string representing the description of the race
+     */
     public String getDescription() {
         return description;
     }
-
+    
+    /**
+     * Gets the raceId 
+     * @return raceId An integer representing the raceId of the race
+     */
     public int getRaceId() {
         return raceId;
     }
-
-    public int getNumberOfStagesInArray() {
+    
+    /**
+     * Gets the number of stages in the race
+     * @return numberOfStages An integer representing the number of stages within the race
+     */
+    public int getNumberOfStagesInRace() {
         return numberOfStages;
     }
     
+    /**
+     * Gets the total length of all stages in a race
+     * @return totalLength An integer that is the sum of all the stage lengths in the race
+     */
     public double getTotalLength() {
         return totalLength;
     }
     
+    /**
+     * Gets the list of race stages
+     * @return raceStageList A list that contains all of the stages within the race
+     */
     public List<Stage> getRaceStageList() {
     	return raceStageList;
     }
-//    public ArrayList<Stage> getRaceStagesArrayList() {
-//        return raceStagesArrayList;
-//    }
 
     // Setters 
-
+    
+    /**
+     * Sets name of the race  
+     * @param name A string representing the name of the race
+     */
     public void setName(String name) {
         this.name = name;
     }
+    
+    /**
+     * Sets the description of the race
+     * @param description A string representing the description of the race
+     */
     public void setDescription(String description) {
         this.description = description;
     }
+    
+    /**
+     * Sets the raceId of the race
+     * @param raceId An integer representing the raceId of the race
+     */
     public void setRaceId(int raceId) {
         this.raceId = raceId;
     }
+    
+    /**
+     * Sets the number of stages of the race
+     * @param numberOfStages An integer representing the number of stages within the race
+     */
     public void setNumberOfStages(int numberOfStages) {
         this.numberOfStages = numberOfStages;
     }
-//    public void setRaceStagesArrayList(ArrayList<Stage> raceStagesArrayList) {
-//        this.raceStagesArrayList = raceStagesArrayList;
-//    }
-
+    
+    /**
+     * Sets the race stage list
+     * @param raceStageList A list that contains all of the stages within the race
+     */
     public void setRaceStageList(List<Stage> raceStageList) {
     	this.raceStageList = raceStageList;
     }
-    
-    // Methods
-
-//    public void addStage(Stage stage){
-//        this.raceStagesArrayList.add(stage);
-//    }
-
-    // add stages to
 
 }
