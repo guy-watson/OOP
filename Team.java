e41462806e2b5a66554b13811a2c4d04297d0634
guy-watson <95031920+guy-watson@users.noreply.github.com>
2022-04-03 14:51:57 +0100

    Update Team.java

--- Team.java
+++ Team.java
@@ -3,84 +3,112 @@ package cycling;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>Team is a public class that holds the team object information.<h1>
+ * 
+ * 
+ * <p>
+ * Team contains each teamId, name, description and a list of the riders with the team.
+ * The team object contains the rider objects. The class contains a constructor that creates
+ * team objects as well as getters, setters and methods for the object attributes
+ * <p>
+ *
+ */
+
 public class Team {
-// Attributes 
+    // Attributes 
 
-  private int teamId;
-  private String name;
-  private String description;
-  static int count = 0;
-  private List<Rider> teamRidersList = new ArrayList<Rider>();
-  private Rider[] teamRiders = new Rider[1000];
+    private int teamId; // the unique id of the team
+    private String name; // name of the team
+    private String description; // description of the team
+    static int count = 0;
+    private List < Rider > teamRidersList = new ArrayList < > (); // array list of all riders within the team
 
-  /**
-		* Creates a team with name and description.
-		* <p>
-		* The state of this MiniCyclingPortalInterface must be unchanged if any
-		* exceptions are thrown.
-		*
-		* @param name The identifier name of the team.
-		* @param description A description of the team.
-		* @return The ID of the created team.
-		* @throws IllegalNameException If the name already exists in the platform.
-		* @throws InvalidNameException If the new name is null, empty, has more than
-		* 30 characters.
-		*/
-    //if(teamArray.contains(name) {
-    // throw exception
-    //}
+    // Constructor 
 
+    /**
+     * <h1>Team Constructor</h1>
+     * Team constructor creates a team object. It assigns the team with an Id using the count attribute/variable.
+     * @param name The name of the team
+     * @param description The description of the team
+     * @throws InvalidNameException Thrown when attempting to assign a race name null, empty or having more than
+     * the system limit of characters. A name must be a single word
+     * @throws IllegalNameException Thrown when attempting to assign a race name already in use in the system
+     */
+    public Team(String name, String description) {
 
-  Team(String name, String description) throws InvalidNameException, IllegalNameException {
-	  
-    //if (teamArrayList.getAttribute(name).equals(name)) {
-    //    throw IllegalNameException();
-    //}
-  this.name = name;
-  this.description = description;
-  count++;
-  teamId = count;
-  //teamArrayList.add(this);
-  // ***** teamArrayList.add(new Team(name, description);
-  }
-    
+        this.name = name;
+        this.description = description;
+        count++;
+        teamId = count;
+
+    }
+
     // Getters
 
+    /**
+     * Gets the teamId
+     * @return An integer representing the unique id of the team
+     */
     public int getTeamId() {
         return teamId;
     }
 
+    /**
+     * Gets the name of the team
+     * @return string representing the name of the team
+     */
     public String getName() {
         return name;
-        }
-    
+    }
+
+    /**
+     * Gets the description of the team
+     * @return string representing the description of the team
+     */
     public String getDescription() {
         return description;
-    }  
-	public Rider[] getTeamRiders() {
-		return teamRiders;
-	}
-	public List<Rider> getTeamRidersList() {
-		return teamRidersList;
-	}
-    
+    }
+
+    /**
+     * Gets an array list that contains the riders in the team
+     * @return An array list containing the riders in the team 
+     */
+    public List < Rider > getTeamRidersList() {
+        return teamRidersList;
+    }
+
     // Setters 
-    
+
+    /**
+     * Sets the teamId
+     * @param An integer that represents the id of the team
+     */
     public void setTeamId(int teamId) {
         this.teamId = teamId;
     }
+
+    /**
+     * Sets the name of the team
+     * @param A string representing the name of the team
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * Sets the description of the team
+     * @param A string representing the description of the team
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-    
-	public void setTeamRiders(Rider[] teamRiders) {
-		this.teamRiders = teamRiders;
-	} 
-	
-	public void setTeamRidersList(List<Rider> teamRidersList) {
-		this.teamRidersList = teamRidersList;
-	}
+
+    /**
+     * Sets team riders list 
+     * @param A list that contains all of the riders within the team
+     */
+    public void setTeamRidersList(List < Rider > teamRidersList) {
+        this.teamRidersList = teamRidersList;
+    }
 }
