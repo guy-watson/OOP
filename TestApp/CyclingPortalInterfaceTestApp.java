e376295ec2ba11f2f4776e146c801b156dd875e9
guy-watson <95031920+guy-watson@users.noreply.github.com>
2022-04-06 21:24:50 +0100

    Update CyclingPortalInterfaceTestApp.java

--- TestApp/CyclingPortalInterfaceTestApp.java
+++ TestApp/CyclingPortalInterfaceTestApp.java
@@ -1,15 +1,24 @@
+package test;
+
 import java.io.*;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 
 import cycling.CyclingPortal;
 import cycling.CyclingPortalInterface;
+import cycling.DuplicatedResultException;
 import cycling.IDNotRecognisedException;
 import cycling.IllegalNameException;
+import cycling.InvalidCheckpointsException;
 import cycling.InvalidLengthException;
+import cycling.InvalidLocationException;
 import cycling.InvalidNameException;
+import cycling.InvalidStageStateException;
+import cycling.InvalidStageTypeException;
 import cycling.MiniCyclingPortalInterface;
 import cycling.NameNotRecognisedException;
+import cycling.SegmentType;
 import cycling.StageType;
 
 /**
@@ -41,20 +50,25 @@ public class CyclingPortalInterfaceTestApp {
 	 * @throws IDNotRecognisedException
 	 * @throws InvalidLengthException
 	 * @throws NameNotRecognisedException
+	 * @throws InvalidStageTypeException 
+	 * @throws InvalidStageStateException 
+	 * @throws InvalidLocationException 
+	 * @throws InvalidCheckpointsException 
+	 * @throws DuplicatedResultException 
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IllegalNameException, InvalidNameException, IDNotRecognisedException, InvalidLengthException, NameNotRecognisedException {
+	public static void main(String[] args) throws IllegalNameException, InvalidNameException, IDNotRecognisedException, InvalidLengthException, NameNotRecognisedException, InvalidLocationException, InvalidStageStateException, InvalidStageTypeException, DuplicatedResultException, InvalidCheckpointsException, IOException, ClassNotFoundException {
 		System.out.println("The system compiled and started the execution...");
 
 		//MiniCyclingPortalInterface portal = MiniCyclingPortalInterfaceal();
 		CyclingPortalInterface portal = new CyclingPortal();
 
-		assert (portal.getRaceIds().length == 0)
-				: "Innitial SocialMediaPlatform not empty as required or not returning an empty array.";
+		
+		
+		//assert (portal.getRaceIds().length == 0)
+		//		: "Initial SocialMediaPlatform not empty as required or not returning an empty array.";
 
-		//for (Integer i = 0;i <= 100; i++) {
-			//portal.createRace("poo", "wheely bike race");
-			//portal.createRace("poo", "wheely bike race");
-		//}
 		
 		portal.createRace("pooa", "wheely bike race");
 		portal.createRace("poob", "wheely bike race");
@@ -64,101 +78,114 @@ public class CyclingPortalInterfaceTestApp {
 		portal.createRace("poof", "wheely bike race");
 		portal.createRace("poog", "wheely bike race");
 		portal.createRace("pooh", "wheely bike race");
-		System.out.println("OO");
-		System.out.println(portal.viewRaceDetails(1));
-		portal.removeRaceByName("skrttttt");
-		System.out.println("PP");
-		System.out.println("------------------------------------------------------------");
+		
 		String testRaceIds = Arrays.toString(portal.getRaceIds());
 		System.out.println(testRaceIds);
-		System.out.println("Hello");
-		System.out.println("------------------------------------------------------------");
-
-		//LocalDateTime datetime1 = LocalDateTime.of(2017, 1, 14, 10, 34);  
-	//StageType flatStage = StageType.FLAT;
-		//portal.addStageToRace(1, "race1", "stage1", 15.3, datetime1, flatStage);
-		//portal.addStageToRace(1, "race1", "stage1", 15.3, datetime1, flatStage);
-		//portal.addStageToRace(1, "race1", "stage1", 15.3, datetime1, flatStage);
-		//System.out.println(portal.viewRaceDetails(3));
 		
 		
-		//this needs to be fixed 
-		//portal.getRaceStages(1);
+		System.out.println(portal.viewRaceDetails(1));
 		
+		LocalDateTime datetime1 = LocalDateTime.of(2017, 1, 14, 10, 34);  
+		StageType flatStage = StageType.FLAT;
+		StageType timeTrial = StageType.TT;
+		portal.addStageToRace(1, "race1", "stage1", 15.3, datetime1, flatStage);
+		portal.addStageToRace(1, "race2", "stage1", 15.3, datetime1, flatStage);
+		portal.addStageToRace(1, "race3", "stage1", 15.3, datetime1, flatStage);
 		
-		//System.out.println(portal.getStageLength(1));
-
-		//System.out.println(portal.viewRaceDetails(1));
-
-		//portal.createTeam("CycleTeamm", "Amazing cycling team!");
-		//portal.createTeam("cycelteammm", "Amazing cycling team!");
-		//String wowza = Arrays.toString(portal.getTeams());
-		//System.out.println(wowza);
-		//System.out.println("HELLO");
-		//portal.removeTeam(1);
-
-		//String wowza2 = Arrays.toString(portal.getTeams());
+		System.out.println(portal.viewRaceDetails(1));
+		portal.getRaceStages(1);
+		System.out.println(portal.getNumberOfStages(1));
+		System.out.println(portal.getStageLength(1));
+		portal.createTeam("WHEELZ", "Amazing cycling team!");
+		portal.createTeam("SQUAREZ", "Amazing cycling team2!");
+		String wowza = Arrays.toString(portal.getTeams());
+		System.out.println(wowza);
+		System.out.println("HELLLLLLLLLLLLLLLLLLLLLLLO");
+		portal.createRider(1, "moo", 2002);
+		portal.createRider(1, "moo222", 2002);
+	//	String wowza1 = Arrays.toString(portal.getTeamRiders(1));
+		//System.out.println(wowza1);
+		
+		//portal.removeRider(1);
+		//System.out.println("HELLLLLLLLLLLLLLLLLLLLLLLO");
+		//String wowza2 = Arrays.toString(portal.getTeamRiders(1));
 		//System.out.println(wowza2);
-		
-		
-		//portal.removeRaceById(3);
-		//portal.removeRaceById(11);
-		String testRaceIds1 = Arrays.toString(portal.getRaceIds());
-		System.out.println(testRaceIds1);
-
-		
-		
-		//.out.println("------------------------------------------------------------");
+		//portal.removeRaceById(2);
 		//String testRaceIds2 = Arrays.toString(portal.getRaceIds());
 		//System.out.println(testRaceIds2);
-		//System.out.println("------------------------------------------------------------");
 		
-		//portal.createRider(2, "lancearmstrong", 2000);
-		//String wowza3 = Arrays.toString(portal.getTeamRiders(1));
-		//System.out.println(wowza3);
-		
-		portal.getNumberOfStages(1);
-		System.out.println("there are " + portal.getNumberOfStages(1) + " stages");
-		System.out.println("------------------------------------------------------------");
-		//String wowza4 = Arrays.toString(portal.getTeams());
-		//System.out.println(wowza4);		
-		//portal.getTeamRiders(2);
-		//System.out.println("------------------------------------------------------------");
-		
-		portal.createTeam("WATSONTEAM", "VERY COOL AMAZING CYCLING TEAM");
-		portal.createTeam("DAVIDTEAM", "VERY NOT COOL NOT AMAZING CYCLING TEAM");
+		SegmentType c4 = SegmentType.C4;
+		portal.addCategorizedClimbToStage(1, 43.4, c4, 34.3, 45.2);
+		portal.addCategorizedClimbToStage(1, 43.4, c4, 34.3, 45.2);
+		portal.addCategorizedClimbToStage(1, 43.4, c4, 34.3, 45.2);
+		portal.addIntermediateSprintToStage(1, 10.2);
 
-		portal.createRider(1, "GARY", 2000);
-		portal.createRider(1, "GARY GLITTER", 2000);
-		portal.createRider(1, "MIKE TYSON", 2000);
-		portal.createRider(1, "KIM KARDASHIAN", 2000);
-		portal.createRider(1, "RUMER WILLIS", 2000);
+		String testss = Arrays.toString(portal.getStageSegments(1));
+		System.out.println(testss + "stage segments b4");
+		portal.removeSegment(3);
+		String testsss = Arrays.toString(portal.getStageSegments(1));
+		System.out.println(testsss + "stage segments after"); 
+	
 		
-		
-		
-		//portal.removeRider(1); - BREOKE
-		
-		
-		//String listOfTeams = Arrays.toString(portal.getTeamRiders(1));
-		//System.out.println(listOfTeams);
-		
-		LocalDateTime datetime2 = LocalDateTime.of(2017, 1, 14, 10, 34);  
-		StageType flatStage2 = StageType.FLAT;
-		portal.addStageToRace(6,"thisisthestagename", "testtoaddstagetorace", 14.4, datetime2, flatStage2);
-		portal.addStageToRace(6,"thisisthestagenamee", "testtoaddstagetorace", 14.4, datetime2, flatStage2);
-		portal.addStageToRace(6,"thisisthestagenameee", "testtoaddstagetorace", 14.4, datetime2, flatStage2);
-		portal.addStageToRace(6,"thisisthestagenameeee", "testtoaddstagetorace", 14.4, datetime2, flatStage2);
-		portal.addStageToRace(6,"thisisthestagenameeeee", "testtoaddstagetorace", 14.4, datetime2, flatStage2);
-		portal.addStageToRace(6,"thisisthestagenameeeeee", "testtoaddstagetorace", 14.4, datetime2, flatStage2);
-		
-		System.out.println(portal.getStageLength(1));
-		
-		
+		LocalTime theMFTime = LocalTime.of(23, 23, 23);
+		LocalTime theM1FTime = LocalTime.of(23, 1, 23);
+		portal.registerRiderResultsInStage(2, 1, theMFTime);
+		portal.registerRiderResultsInStage(3, 2, theM1FTime);
+		portal.registerRiderResultsInStage(2, 3, theMFTime);
+		portal.registerRiderResultsInStage(2, 4, theMFTime);
+		portal.registerRiderResultsInStage(3, 5, theMFTime);
+		portal.registerRiderResultsInStage(2, 1, theMFTime);
 
-		System.out.println("there are " + portal.getNumberOfStages(6) + " stages");
+		String LALALALA = Arrays.toString(portal.getRiderResultsInStage(2, 1));
+		System.out.println(LALALALA);
 
+		String LALALALA1 = Arrays.toString(portal.getRiderResultsInStage(2, 3));
 
+		System.out.println(LALALALA1);
+		// IT WORKS LESSSSSSSS GOOOOOOOOOOO
+		
+		portal.saveCyclingPortal("savesave.ser");
+		System.out.println(portal.getNumberOfStages(1));
 
+		portal.loadCyclingPortal("savesave.ser");
+		System.out.println(portal.getNumberOfStages(1));
+		
+		portal.getRaceIds();
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
